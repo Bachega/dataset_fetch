a07ce47a7ce197328ed9dad71679c00a90f0d40f
Guilherme Bachega Gomes <guizobachegagomes@gmail.com>
2025-01-24 11:03:58 -0300

    added timeout function (works on linux)

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,5 +4,6 @@ from .arff_to_csv import arff_to_csv
 from .preprocess_data import preprocess_data
 from .test_mfe import test_mfe
 from .test_scorer import test_scorer
+from .timeout_exception import run_with_timeout
 
-__all__ = ['getTPRFPR', 'getTrainingScores', 'arff_to_csv', 'preprocess_data', 'test_mfe', 'test_scorer']
+__all__ = ['getTPRFPR', 'getTrainingScores', 'arff_to_csv', 'preprocess_data', 'test_mfe', 'test_scorer', 'run_with_timeout']
