14763c71c0b24aecd5da081b1551f6be40b2a0d5
Guilherme Bachega Gomes <guizobachegagomes@gmail.com>
2025-01-26 03:51:12 -0300

    implemented fetch (download) datasets

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,5 +5,6 @@ from .preprocess_data import preprocess_data
 from .test_mfe import test_mfe
 from .test_scorer import test_scorer
 from .run_with_timeout import run_with_timeout
+from .run_tests import run_tests
 
-__all__ = ['getTPRFPR', 'getTrainingScores', 'arff_to_csv', 'preprocess_data', 'test_mfe', 'test_scorer', 'run_with_timeout']
+__all__ = ['getTPRFPR', 'getTrainingScores', 'arff_to_csv', 'preprocess_data', 'test_mfe', 'test_scorer', 'run_with_timeout', 'run_tests']
